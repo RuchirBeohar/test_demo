78412f3ae834a97914f7fe273b99121cff5fe017
Ruchir Beohar <ruchirbeohar2000@gmail.com>
2023-01-18 13:26:36 +0530

    updating a python file

--- pythonfile.py
+++ pythonfile.py
@@ -1,1 +1,6 @@
 print("After cloning the file we will add it to repository on githu from our system or desktop...)
+in = input("Please enter age:")
+if in==ten:
+    print("you enter ten")
+else:
+    print("you enter other")    
